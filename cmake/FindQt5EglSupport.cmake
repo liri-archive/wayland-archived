7609f0cf859bd3f46fd4b7e22f0fc6ec2a937e6f
Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
2016-12-11 12:39:13 +0100

    Support modularized QtPlatformSupport

--- cmake/FindQt5EglSupport.cmake
+++ cmake/FindQt5EglSupport.cmake
@@ -1,29 +1,29 @@
 #.rst:
-# FindQt5PlatformSupport
+# FindQt5EglSupport
 # -------
 #
-# Try to find Qt5PlatformSupport on a Unix system.
+# Try to find Qt5EglSupport on a Unix system.
 #
 # This will define the following variables:
 #
-# ``Qt5PlatformSupport_FOUND``
-#     True if (the requested version of) Qt5PlatformSupport is available
-# ``Qt5PlatformSupport_VERSION``
-#     The version of Qt5PlatformSupport
-# ``Qt5PlatformSupport_LIBRARIES``
-#     This can be passed to target_link_libraries() instead of the ``Qt5PlatformSupport::Qt5PlatformSupport``
+# ``Qt5EglSupport_FOUND``
+#     True if (the requested version of) Qt5EglSupport is available
+# ``Qt5EglSupport_VERSION``
+#     The version of Qt5EglSupport
+# ``Qt5EglSupport_LIBRARIES``
+#     This can be passed to target_link_libraries() instead of the ``Qt5EglSupport::Qt5EglSupport``
 #     target
-# ``Qt5PlatformSupport_INCLUDE_DIRS``
+# ``Qt5EglSupport_INCLUDE_DIRS``
 #     This should be passed to target_include_directories() if the target is not
 #     used for linking
-# ``Qt5PlatformSupport_DEFINITIONS``
+# ``Qt5EglSupport_DEFINITIONS``
 #     This should be passed to target_compile_options() if the target is not
 #     used for linking
 #
-# If ``Qt5PlatformSupport_FOUND`` is TRUE, it will also define the following imported target:
+# If ``Qt5EglSupport_FOUND`` is TRUE, it will also define the following imported target:
 #
-# ``Qt5PlatformSupport::Qt5PlatformSupport``
-#     The Qt5PlatformSupport library
+# ``Qt5EglSupport::Qt5EglSupport``
+#     The Qt5EglSupport library
 #
 # In general we recommend using the imported target, as it is easier to use.
 # Bear in mind, however, that if the target is in the link interface of an
@@ -32,6 +32,8 @@
 #=============================================================================
 # Copyright 2014 Alex Merry <alex.merry@kde.org>
 # Copyright 2014 Martin Gräßlin <mgraesslin@kde.org>
+# Copyright 2016 Takahiro Hashimoto <kenya888@gmail.com>
+# Copyright 2016 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -58,65 +60,64 @@
 #=============================================================================
 
 if(CMAKE_VERSION VERSION_LESS 2.8.12)
-    message(FATAL_ERROR "CMake 2.8.12 is required by FindQt5PlatformSupport.cmake")
+    message(FATAL_ERROR "CMake 2.8.12 is required by FindQt5EglSupport.cmake")
 endif()
 if(CMAKE_MINIMUM_REQUIRED_VERSION VERSION_LESS 2.8.12)
-    message(AUTHOR_WARNING "Your project should require at least CMake 2.8.12 to use FindQt5PlatformSupport.cmake")
+    message(AUTHOR_WARNING "Your project should require at least CMake 2.8.12 to use FindQt5EglSupport.cmake")
 endif()
 
 # Use pkg-config to get the directories and then use these values
 # in the FIND_PATH() and FIND_LIBRARY() calls
 find_package(PkgConfig)
-pkg_check_modules(PKG_Qt5PlatformSupport QUIET Qt5Gui)
+pkg_check_modules(PKG_Qt5EglSupport QUIET Qt5Gui)
 
-set(Qt5PlatformSupport_DEFINITIONS ${PKG_Qt5PlatformSupport_CFLAGS_OTHER})
-set(Qt5PlatformSupport_VERSION ${PKG_Qt5PlatformSupport_VERSION})
+set(Qt5EglSupport_DEFINITIONS ${PKG_Qt5EglSupport_CFLAGS_OTHER})
+set(Qt5EglSupport_VERSION ${PKG_Qt5EglSupport_VERSION})
 
-find_path(Qt5PlatformSupport_INCLUDE_DIR
+find_path(Qt5EglSupport_INCLUDE_DIR
     NAMES
-        QtPlatformSupport/private/qfontconfigdatabase_p.h
+        QtEglSupport/private/qt_egl_p.h
     HINTS
-        ${PKG_Qt5PlatformSupport_INCLUDEDIR}/QtPlatformSupport/${PKG_Qt5PlatformSupport_VERSION}/
+        ${PKG_Qt5EglSupport_INCLUDEDIR}/QtEglSupport/${PKG_Qt5EglSupport_VERSION}/
 )
-find_library(Qt5PlatformSupport_LIBRARY
+find_library(Qt5EglSupport_LIBRARY
     NAMES
-        Qt5PlatformSupport
+        Qt5EglSupport
     HINTS
-        ${PKG_Qt5PlatformSupport_LIBRARY_DIRS}
+        ${PKG_Qt5EglSupport_LIBRARY_DIRS}
 )
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Qt5PlatformSupport
+find_package_handle_standard_args(Qt5EglSupport
     FOUND_VAR
-        Qt5PlatformSupport_FOUND
+        Qt5EglSupport_FOUND
     REQUIRED_VARS
-        Qt5PlatformSupport_LIBRARY
-        Qt5PlatformSupport_INCLUDE_DIR
+        Qt5EglSupport_LIBRARY
+        Qt5EglSupport_INCLUDE_DIR
     VERSION_VAR
-        Qt5PlatformSupport_VERSION
+        Qt5EglSupport_VERSION
 )
 
-if(Qt5PlatformSupport_FOUND AND NOT TARGET Qt5PlatformSupport::Qt5PlatformSupport)
-    add_library(Qt5PlatformSupport::Qt5PlatformSupport UNKNOWN IMPORTED)
-    set_target_properties(Qt5PlatformSupport::Qt5PlatformSupport PROPERTIES
-	IMPORTED_LINK_DEPENDENT_LIBRARIES "${PKG_Qt5PlatformSupport_STATIC_LIBRARIES}"
-        IMPORTED_LOCATION "${Qt5PlatformSupport_LIBRARY}"
-        INTERFACE_COMPILE_OPTIONS "${Qt5PlatformSupport_DEFINITIONS}"
-        INTERFACE_INCLUDE_DIRECTORIES "${Qt5PlatformSupport_INCLUDE_DIR}"
+if(Qt5EglSupport_FOUND AND NOT TARGET Qt5EglSupport::Qt5EglSupport)
+    add_library(Qt5EglSupport::Qt5EglSupport UNKNOWN IMPORTED)
+    set_target_properties(Qt5EglSupport::Qt5EglSupport PROPERTIES
+        IMPORTED_LOCATION "${Qt5EglSupport_LIBRARY}"
+        INTERFACE_COMPILE_OPTIONS "${Qt5EglSupport_DEFINITIONS}"
+        INTERFACE_INCLUDE_DIRECTORIES "${Qt5EglSupport_INCLUDE_DIR}"
     )
 endif()
 
-mark_as_advanced(Qt5PlatformSupport_LIBRARY Qt5PlatformSupport_INCLUDE_DIR)
+mark_as_advanced(Qt5EglSupport_LIBRARY Qt5EglSupport_INCLUDE_DIR)
 
 # compatibility variables
-set(Qt5PlatformSupport_LIBRARIES ${Qt5PlatformSupport_LIBRARY})
-set(Qt5PlatformSupport_INCLUDE_DIRS ${Qt5PlatformSupport_INCLUDE_DIR})
-set(Qt5PlatformSupport_VERSION_STRING ${Qt5PlatformSupport_VERSION})
+set(Qt5EglSupport_LIBRARIES ${Qt5EglSupport_LIBRARY})
+set(Qt5EglSupport_INCLUDE_DIRS ${Qt5EglSupport_INCLUDE_DIR})
+set(Qt5EglSupport_VERSION_STRING ${Qt5EglSupport_VERSION})
 
 
 include(FeatureSummary)
-set_package_properties(Qt5PlatformSupport PROPERTIES
+set_package_properties(Qt5EglSupport PROPERTIES
     URL "http://www.qt.io"
-    DESCRIPTION "Qt PlatformSupport module."
+    DESCRIPTION "Qt ThemeSupport module."
 )
 
