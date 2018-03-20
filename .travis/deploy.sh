8f1b831a913b91396cbee8a9ea3ec29dc9e648c7
Pier Luigi Fiorini <pierluigi.fiorini@liri.io>
2018-03-20 23:56:15 +0100

    Deploy with scp
    Apparently ftp no longer works with Travis CI.
    Host the artifacts via ssh instead.

---
+++ .travis/deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+# This file is part of Liri.
+#
+# Copyright (C) 2018 Pier Luigi Fiorini <pierluigi.fiorini@gmail.com>
+#
+# $BEGIN_LICENSE:MIT$
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+#
+# $END_LICENSE$
+#
+
+# Deployment procedure as seen in https://oncletom.io/2016/travis-ssh-deploy/
+
+filename=$1
+
+if [ -z "$filename" ]; then
+    echo "Usage: $0 [filename]"
+    exit 1
+fi
+
+destfilename=$(basename $filename)
+
+ssh-keyscan $DEPLOY_HOST 2>&1 | tee -a $HOME/.ssh/known_hosts
+openssl aes-256-cbc -K $encrypted_fd8ea058a4e4_key -iv $encrypted_fd8ea058a4e4_iv -in .travis/github_deploy_key_liri_ci.enc -out /tmp/deploy_rsa -d
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/deploy_rsa
+ssh-add /tmp/deploy_rsa
+scp $filename $DEPLOY_USER@$DEPLOY_HOST:$TRAVIS_BRANCH/$destfilename
